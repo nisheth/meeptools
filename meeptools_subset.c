abc3a313dad4b81f31001fa1ea3bb6ab621ebcd9
Vishal N. Koparde, Ph.D <vnkoparde@vcu.edu>
2015-02-09 15:30:07 -0500

    testing pushing

--- meeptools_subset.c
+++ meeptools_subset.c
@@ -1,7 +1,7 @@
 #include "meeptools_subset.h"
 static int meeptools_subset_usage()
 {
-    fprintf(stderr,"In function %s\n",__func__);
+    fprintf(stderr,"In function %s\n This is still under development.\n",__func__);
     return 1;
 }
 int meeptools_subset(int argc, char *argv[])
